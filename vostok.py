a151d5bebd03e95ae19a06562d04947254e0f455
Moksh <moksh.kumar2025@vitstudent.ac.in>
2025-09-21 11:26:46 +0530

    sample file for commit message

--- vostok.py
+++ vostok.py
@@ -1,2 +1,16 @@
 import cv2 as cv
 import mediapipe as mp
+img  = cv.imread('vostok.jpg')
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
+capture = cv.videocapture(0)
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow("Hand", frame)
+    if cv.wait(key) & 0xFF == ord('q'):
+        break
+capture.release()
+cv.destroyAllWindows()
